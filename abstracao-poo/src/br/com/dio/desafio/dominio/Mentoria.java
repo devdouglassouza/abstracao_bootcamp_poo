a2bb673ddc0ec158130319a54fb803fc85d3ade7
Douglas Souza <developer.douglas@outlook.com>
2024-06-25 13:41:22 -0300

    Mentoria e Curso

--- abstracao-poo/src/br/com/dio/desafio/dominio/Mentoria.java
+++ abstracao-poo/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,10 +1,50 @@
 package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Mentoria {
-    String titulo;
-    String descricao;
-    LocalDate data;
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria() {
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    // Método para obter a data formatada
+    public String getDataFormatada() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return this.data.format(formatter);
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + getDataFormatada() + "]";
+    }
 
 }
